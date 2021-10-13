ea8ea529423ef42f416115160ea3df8efdf95d64
Theo Chatzimichos <theo.chatzimichos@jobandtalent.com>
2021-10-13 12:36:44 +0200

    Prepare for the backend weekly kubernetes presentation demo

--- app.py
+++ app.py
@@ -13,55 +13,58 @@ color_codes = {
     "blue": "#2980b9",
     "blue2": "#30336b",
     "pink": "#be2edd",
-    "darkblue": "#130f40"
+    "darkblue": "#130f40",
+    "white": "#e4e4e4",
 }
 
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
-# Get color from Environment variable
-COLOR_FROM_ENV = os.environ.get('APP_COLOR')
-# Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
+# Get background color from Environment variable
+BG_COLOR_FROM_ENV = os.environ.get('BG_COLOR')
+# Default background color
+BG_COLOR = "blue"
+
+# Get text color from Environment variable
+TEXT_COLOR_FROM_ENV = os.environ.get('TEXT_COLOR')
+# Default background color
+TEXT_COLOR = "white"
+
 
 
 @app.route("/")
 def main():
     # return 'Hello'
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[COLOR])
+    return render_template('hello.html', name=socket.gethostname(), bg_color=color_codes[BG_COLOR], text_color=color_codes[TEXT_COLOR])
 
 
 if __name__ == "__main__":
 
-    print(" This is a sample web application that displays a colored background. \n"
-          " A color can be specified in two ways. \n"
-          "\n"
-          " 1. As a command line argument with --color as the argument. Accepts one of " + SUPPORTED_COLORS + " \n"
-          " 2. As an Environment variable APP_COLOR. Accepts one of " + SUPPORTED_COLORS + " \n"
-          " 3. If none of the above then a random color is picked from the above list. \n"
-          " Note: Command line argument precedes over environment variable.\n"
-          "\n"
+    print(" This is a sample web application that displays a colored background and text. \n"
+          " A color can be specified as an Environment variable BG_COLOR and TEXT_COLOR. Accepts one of " + SUPPORTED_COLORS + " \n"
           "")
 
-    # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
+    if BG_COLOR_FROM_ENV:
+        print("No Command line argument for BG_COLOR. Color from environment variable " + BG_COLOR_FROM_ENV)
+        BG_COLOR = BG_COLOR_FROM_ENV
     else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
+        print("No command line argument or environment variable for BG_COLOR. Using the default color " + BG_COLOR)
 
     # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
+    if BG_COLOR not in color_codes:
+        print("Color not supported. Received '" + BG_COLOR + "' expected one of " + SUPPORTED_COLORS)
         exit(1)
+
+    if TEXT_COLOR_FROM_ENV:
+        print("No Command line argument for TEXT_COLOR. Color from environment variable " + TEXT_COLOR_FROM_ENV)
+        TEXT_COLOR = TEXT_COLOR_FROM_ENV
+    else:
+        print("No command line argument or environment variable for TEXT_COLOR. Using the default color " + TEXT_COLOR)
+
+    # Check if input color is a supported one
+    if TEXT_COLOR not in color_codes:
+        print("Color not supported. Received '" + TEXT_COLOR + "' expected one of " + SUPPORTED_COLORS)
+        exit(1)
+
 
     # Run Flask Application
     app.run(host="0.0.0.0", port=8080)
